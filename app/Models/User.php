55280793005f3d237b70c4edcc36f21f148c1385
DizkyHenrico <96405902+DizkyHenrico@users.noreply.github.com>
2023-06-16 06:30:51 +0700

    api : auth, read, search data

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,12 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use App\Models\Todo;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -41,6 +40,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'is_admin' => 'boolean'
     ];
     public function todos()
     {
